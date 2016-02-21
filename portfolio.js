ec9c61890ac4bbba8bf4eb6c212f4ec56536829a
madzik <magdalena.pal90@gmail.com>
2016-02-21 22:46:07 +0100

    Fix sticky navbar

--- portfolio.js
+++ portfolio.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('a[href^="#"]').click(function() {
         var hash = $(this).attr('href');
         $('html, body').animate({
-            scrollTop: $(hash).offset().top
+            scrollTop: $(hash).offset().top - 50
         }, 500);
         return false;
     });
