1ef702fabfe86f6b80e2f652f6603f4664bf4db0
madzik <magdalena.pal90@gmail.com>
2016-02-21 12:38:53 +0100

    Add sticky menu

--- portfolio.js
+++ portfolio.js
@@ -8,5 +8,23 @@ $(document).ready(function() {
     });
 });
 
+$(document).ready(function() {
+var stickyNavTop = $('#navigation').offset().top;
+ 
+var stickyNav = function(){
+var scrollTop = $(window).scrollTop();
+      
+if (scrollTop > stickyNavTop) { 
+    $('#navigation').addClass('sticky');
+} else {
+    $('#navigation').removeClass('sticky'); 
+}
+};
+ 
+stickyNav();
+ 
+$(window).scroll(function() {
+    stickyNav();
+});
 
  
